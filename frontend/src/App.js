980d4d0238c5b07707ed9fffcf4d2f1c222f5784
kripajha <kripashankarjha9@gmail.com>
2025-11-02 15:33:38 +0530

    fix: Update backend configuration and add gitignore

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import CareersPage from './pages/CareersPage';
 import JobDetailPage from './pages/JobDetailPage';
 import AdminLogin from './pages/AdminLogin';
@@ -7,7 +7,7 @@ import AdminDashboard from './pages/AdminDashboard';
 
 function App() {
   return (
-    <Router>
+    <BrowserRouter>
       <div className="min-h-screen bg-gray-50">
         <Routes>
           <Route path="/" element={<CareersPage />} />
@@ -18,7 +18,7 @@ function App() {
           <Route path="/admin" element={<AdminDashboard />} />
         </Routes>
       </div>
-    </Router>
+    </BrowserRouter>
   );
 }
 
